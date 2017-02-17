348d5aabf80fcb23c415d510e044ac8e2343777f
Bill Wright <billwright93@googmail.com>
2017-02-17 17:41:09 +0000

    After conversation with Ben, I removed H0 factor from HA(a) function as gamma2 should have a H0^2 factor within to cancel the HA^-2 factor outside. gamma2 part of ode2 equation, as well as h0 normalisation of p1 in mu and gamma2, remains the same. I then changed to new file kernelb4 to combine the 1st and 2nd order solvers into a single function. Ben explained that I could just calculate D1(a, k1) and D1(a, k2) at the same time as D2(a, k, k1, k2) and D1(a, k). Doing so removes the need for any 2d interpolation. Code now runs much faster. Hans and I are unsure whether the D1k1*D1k2 should multiply the gamma2 part of the 2nd order equation as well as the (1-k1dotk2) part. We are also not confident about the relative sign of these two parts, or on the precise normalisation of the gamma2 part in terms of a and H(a).

--- kernelb4.cpp
+++ kernelb4.cpp
@@ -1,7 +1,8 @@
 //COMPILE + RUN COMMANDS//
 
-//g++ -I/opt/apps/libs/gsl/2.1/gcc-4.4.7/include -I/users/wrightw/work/cpplibs/headers -L/opt/apps/libs/gsl/2.1/gcc-4.4.7/lib -lgsl -lstdc++ -lgslcblas kernelb32.cpp /users/wrightw/work/cpplibs/cpps/*.cpp
+// g++ -I/opt/apps/libs/gsl/2.1/gcc-4.4.7/include -L/opt/apps/libs/gsl/2.1/gcc-4.4.7/lib -lgsl -lstdc++ -lgslcblas kernelb4.cpp
 // ./a.out
+
 
 
 #include <gsl/gsl_errno.h>
@@ -12,23 +13,17 @@
 #include <math.h>
 #include <stdlib.h>
 #include <iostream>
-#include <vector>
-#include "spline.h"
-
-#include "stdafx.h"
-#include <stdio.h>
-#include "interpolation.h"
 
 
 
 // CONSTANTS //
 const double pipi = 3.1415926535897;
-const	double H0 = (3.09e+19);//70.;//
-double omega0 = 0.99;//0.99;//
+//const	double H0 = (3.09e+19);//70.;//
+double omega0 = 0.25;//0.99;//
 
 
 // MG SWITCHES //
-int MG = 0; //0=GR, 1=f(R), 2=DGP
+int MG = 1; //0=GR, 1=f(R), 2=DGP
 
 
 // SAMPLING RANGES FOR a, k(and k1), x //
@@ -43,28 +38,30 @@ double kmin = 0.01;//0.01;
 double kmax = 10;//0.2;
 
 // Number of x=cos(theta) modes you wish to output between xmin and xmax
-const int Nx = 4;//4; //10
+const int Nx = 4; //10
 double xmin = -1.+1.e-10;
 double xmax = 1.-1.e-10;
 
 
 
 // DECLARING VARIABLES AND ARRAYS //
-double a_arr[Na];
-double k_arr[Nk];
-double D1_arr[Na*Nk];
-
-alglib::real_1d_array aa_arr;
-alglib::real_1d_array kk_arr;
-alglib::real_1d_array D1D1_arr;
 
 //D1/D1_dot(k)
 double D1;
 double dD1;
 
+//D1(k)
+double D1k[Nk*Nx];
+
+//D1(k1)
+double D1k1[Nk*Nx];
+
+//D1(k2)
+double D1k2[Nk*Nx];
+
 //k.L2(k, k1, theta)
 double kL2[Nk*Nx];
-double dkL2[Nk*Nx];
+//double dkL2[Nk*Nx];
 
 
 
@@ -79,17 +76,6 @@ static double pow3(double base){
 
 static double pow4(double base){
 	return pow(base, 4.);}
-
-
-//D1 2d spline interpolation function
-double calc_D1_2d(double a, double k, alglib::real_1d_array& a_arr, alglib::real_1d_array& k_arr, alglib::real_1d_array& D1_arr){
-	// k=x, a=y
-	double v;
-	alglib::spline2dinterpolant s;
-	alglib::spline2dbuildbicubicv(k_arr, k_arr.length(), a_arr, a_arr.length(), D1_arr, 1, s); //1=dimension of D1 values since scalar field
-	v = alglib::spline2dcalc(s, k, a);
-	return v;
-}
 
 
 // Normalized Hubble parameter H, a*H*dH/da, and Omega_M(a) for the background (LCDM)
@@ -184,77 +170,7 @@ int jac (double a, const double D[], double *dfdy, double dfdt[], void *params)
 
 
 
-
-	//// 1ST ORDER ////
-
-  struct param_type3 {
-    double kk;
-  	double OmegaM00;
-  	double par1;
-  	double par2;
-  	double par3;
-		int mmgg;
-  };
-
-  int ode1(double a, const double D[], double dDda[], void *params)
-  {
-  	param_type3 p = *(param_type3 *)(params);
-  	double k = p.kk;
-  	double OmegaM0 = p.OmegaM00;
-  	double p1 = p.par1;
-  	double p2 = p.par2;
-  	double p3 = p.par3;
-		int mg = p.mmgg;
-
-		//Equations for 1st order growth factor D1/dD1(k)
-		dDda[0] = D[1];
-		dDda[1] = -1.*( (3./a) + (dHA(a, OmegaM0)/HA(a, OmegaM0)) )*D[1] + 1.5*(Omega_M(a, OmegaM0)/pow2(a))*mu(a, k, OmegaM0, p1, p2, p3, mg)*D[0];
-
-  	return GSL_SUCCESS;
-  }
-
-
-  // EDIT : Add in new gravity parameters to function's input as required (par1 is the nDGP parameter omega_rc as default)
-  // EDIT : if more than 1 gravity parameter is needed, one must also edit initn function in SPT.h file.
-
-  int solve1(double A, double k, double OmegaM0, double par1, double par2, double par3, int mg)
-  {
-  //#pragma omp parallel for schedule(dynamic)
-
-		// Initial scale factor for solving system of equations
-		double a = a_ini;
-
-		// Einstein de Sitter initial condtions for 1st order growth factor and derivative
-		double D[2] = { 1., 1./a };//{a, 1.}; //
-
-		/*Parameters passed to system of equations */
-	  // EDIT : If more than one gravity parameter is used, add them after p1
-		struct param_type3 my_params1 = {k, OmegaM0, par1, par2, par3, mg};
-
-		gsl_odeiv2_system sys = {ode1, jac, 2, &my_params1};
-
-		//  this gives the system, the method, the initial interval step, the absolute error and the relative error.
-		gsl_odeiv2_driver * d =
-		gsl_odeiv2_driver_alloc_y_new (&sys, gsl_odeiv2_step_rk8pd,
-										1e-6, 1e-6, 0.0);
-
-		int status1 = gsl_odeiv2_driver_apply (d, &a, A, D);
-
-  	/*Allocation of array values */
-		//D1/D1'(k)
-		D1  = D[0];
-		dD1 = D[1];
-
-  	gsl_odeiv2_driver_free(d);
-
-  	return 0;
-  }
-
-
-
-
-
-	///// 2ND ORDER ////
+	///// 1ST + 2ND ORDER ////
 
 	struct param_type4 {
     double kk;
@@ -283,14 +199,22 @@ int jac (double a, const double D[], double *dfdy, double dfdt[], void *params)
 		int mg = p.mmgg;
 
 
-		//Use 2d interpoaltion to find D1 for any (a, k) combination
-		double D1k1 = calc_D1_2d(a, k1, aa_arr, kk_arr, D1D1_arr);
-		double D1k2 = calc_D1_2d(a, k2, aa_arr, kk_arr, D1D1_arr);
+		//Equations for 1st order growth factor D1/dD1(k)
+		dDda[0] = D[1];
+		dDda[1] = -1.*( (3./a) + (dHA(a, OmegaM0)/HA(a, OmegaM0)) )*D[1] + 1.5*(Omega_M(a, OmegaM0)/pow2(a))*mu(a, k, OmegaM0, p1, p2, p3, mg)*D[0];
 
+		//Equations for 1st order growth factor D1/dD1(k1)
+		dDda[2] = D[3];
+		dDda[3] = -1.*( (3./a) + (dHA(a, OmegaM0)/HA(a, OmegaM0)) )*D[3] + 1.5*(Omega_M(a, OmegaM0)/pow2(a))*mu(a, k1, OmegaM0, p1, p2, p3, mg)*D[2];
+
+		//Equations for 1st order growth factor D1/dD1(k2)
+		dDda[4] = D[5];
+		dDda[5] = -1.*( (3./a) + (dHA(a, OmegaM0)/HA(a, OmegaM0)) )*D[5] + 1.5*(Omega_M(a, OmegaM0)/pow2(a))*mu(a, k2, OmegaM0, p1, p2, p3, mg)*D[4];
 
 		//Equations for 2nd order kernels: k.L2(k, k1, theta)
-		dDda[0] = D[1];
-		dDda[1] = -1.*( (3./a) + (dHA(a, OmegaM0)/HA(a, OmegaM0)) )*D[1] + 1.5*(Omega_M(a, OmegaM0)/pow2(a))*mu(a, k, OmegaM0, p1, p2, p3, mg)*D[0] + 1.5*(Omega_M(a, OmegaM0)/pow2(a))*mu(a, k, OmegaM0, p1, p2, p3, mg)*D1k1*D1k2*(1-pow2(k1dotk2)) + ( 2./(pow4(a)*pow2(HA(a, OmegaM0))) )*gamma2(a, OmegaM0, k, k1, k2, p1, p2, p3, mg);
+		dDda[6] = D[7];
+		dDda[7] = -1.*( (3./a) + (dHA(a, OmegaM0)/HA(a, OmegaM0)) )*D[7] + 1.5*(Omega_M(a, OmegaM0)/pow2(a))*mu(a, k, OmegaM0, p1, p2, p3, mg)*D[6]
+		         + 1.5*(Omega_M(a, OmegaM0)/pow2(a))*mu(a, k, OmegaM0, p1, p2, p3, mg)*D[2]*D[4]*(1-pow2(k1dotk2)) + ( 2.*D[2]*D[4]/(pow4(a)*pow2(HA(a, OmegaM0))) )*gamma2(a, OmegaM0, k, k1, k2, p1, p2, p3, mg);
 
   	return GSL_SUCCESS;
   }
@@ -307,7 +231,7 @@ int jac (double a, const double D[], double *dfdy, double dfdt[], void *params)
 			for(int x_num = 0; x_num < Nx; x_num++){
 
 				//x=cos(theta_k,k1)
-				double x = xmin + x_num*(xmax-xmin)/Nx; //0.5;//
+				double x = xmin + x_num*(xmax-xmin)/Nx; //0.5;
 
 				double k2 = sqrt(pow2(k)+pow2(k1)-(2.*k*k1*x));
 
@@ -320,14 +244,14 @@ int jac (double a, const double D[], double *dfdy, double dfdt[], void *params)
 				double a = a_ini;
 
 				// Einstein de Sitter initial condtions for 2nd order
-				double D[2] = {(3./7.)*(1-pow2(k1dotk2)), (6./7.)*(1-pow2(k1dotk2))/a}; //{(3./7.)*(1-pow2(k1dotk2))*pow2(a), (6./7.)*(1-pow2(k1dotk2))*a}; //{ (3./7.)*(1.-pow2(k1dotk2)), (6./7.)*(1.-pow2(k1dotk2))/a };
+				double D[8] = { 1., 1./a, 1., 1./a, 1., 1./a, (3./7.)*(1-pow2(k1dotk2)), (6./7.)*(1-pow2(k1dotk2))/a };//  1., 1./a}; //{(3./7.)*(1-pow2(k1dotk2))*pow2(a), (6./7.)*(1-pow2(k1dotk2))*a}; //{ (3./7.)*(1.-pow2(k1dotk2)), (6./7.)*(1.-pow2(k1dotk2))/a };
 
   			/*Parameters passed to system of equations */
   			// EDIT : If more than one gravity parameter is used, add them after p1
   			struct param_type4 my_params2 = {k, k1, k2, k1dotk2, OmegaM0, par1, par2, par3, mg};
 
 
-  			gsl_odeiv2_system sys = {ode2, jac, 2, &my_params2};
+  			gsl_odeiv2_system sys = {ode2, jac, 8, &my_params2};
 
   			//  this gives the system, the method, the initial interval step, the absolute error and the relative error.
   			gsl_odeiv2_driver * d =
@@ -338,9 +262,18 @@ int jac (double a, const double D[], double *dfdy, double dfdt[], void *params)
 
   			/*Allocation of array values */
 
+				//D1(k)
+				D1k[k1_num*Nx + x_num] = D[0];
+
+				//D1(k1)
+				D1k1[k1_num*Nx + x_num] = D[2];
+
+				//D1(k2)
+				D1k2[k1_num*Nx + x_num] = D[4];
+
 				//(k.L2)/d(k.L2)
-				kL2[k1_num*Nx + x_num]  = D[0];
-				dkL2[k1_num*Nx + x_num] = D[1];
+				kL2[k1_num*Nx + x_num]  = D[6];
+				//dkL2[k1_num*Nx + x_num] = D[7];
 
   			gsl_odeiv2_driver_free(d);
   		}
@@ -359,7 +292,7 @@ int main(int argc, char* argv[]) {
 
 	//output file name
 	//Could create if statements here so that different MG gives different file names
-	const char* output = "kernelb3.dat";
+	const char* output = "kernelb4.dat";
 
 	/* Open output file */
 	FILE* fp = fopen(output, "w");
@@ -368,38 +301,6 @@ int main(int argc, char* argv[]) {
 	double p1, p2, p3, p4, p5;
 
 	// 1ST ORDER //
-
-	for(int a_num = 0 ; a_num <Na; a_num++){
-
-		// Example a value
-		double a_val = a_ini + a_num*(a_fin-a_ini)/Na; //Linear sampling
-		a_arr[a_num] = a_val;
-
-		for(int k_num = 0 ; k_num < Nk;  k_num ++){
-
-			// Example k value
-			//double k = kmin + k_num*(kmax-kmin)/Nk; //Linear sampling
-			double k = kmin * exp(k_num*log(kmax/kmin)/(Nk*1.-1.)); //Exponential sampling
-			k_arr[k_num] = k;
-
-			// Format : solve1(scale factor, k, omega_(total matter), theory parameter1, theory parameter2 ...)
-			solve1(a_val, k, omega0, 0.00001, 1., 1., MG);
-			D1_arr[a_num*Nk + k_num] = D1;
-
-			fprintf(fp,"%e %e %e \n", a_val, k, D1);
-
-			//std::cout << "a, k, D1: " << a_val << " " << k << " " << D1 << '\n';
-
-			}
-
-	}
-
-	aa_arr.setcontent(sizeof(a_arr)/sizeof(*a_arr), a_arr);
-	kk_arr.setcontent(sizeof(k_arr)/sizeof(*k_arr), k_arr);
-	D1D1_arr.setcontent(sizeof(D1_arr)/sizeof(*D1_arr), D1_arr);
-
-	std::cout << "1ST ORDER COMPLETED" << '\n';
-
 	// 2ND ORDER //
 
 	for(int a_num = 0 ; a_num <Na; a_num++){
@@ -414,8 +315,8 @@ int main(int argc, char* argv[]) {
 				//double k = kmin + k_num*(kmax-kmin)/Nk; //Linear sampling
 				double k = kmin * exp(k_num*log(kmax/kmin)/(Nk*1.-1.)); //Exponential sampling
 
-
 				solve2( a_val, k, omega0, 0.00001, 1., 1., MG);
+
 
 				for(int k1_num = 0; k1_num < Nk; k1_num++){
 
@@ -426,7 +327,7 @@ int main(int argc, char* argv[]) {
 					for(int x_num = 0; x_num < Nx; x_num++){
 
 						//Example x=cos(theta_k,k1) value
-						double x = xmin + x_num*(xmax-xmin)/Nx;  //Linear sampling //0.5;//
+						double x = xmin + x_num*(xmax-xmin)/Nx;  //Linear sampling //0.5;
 
 						double k2 = sqrt(pow2(k)+pow2(k1)-2.*k*k1*x);
 
@@ -436,11 +337,11 @@ int main(int argc, char* argv[]) {
 
 
 						//Store 1st and 2nd order values
-						p1 = D1_arr[a_num*Nk + k_num];
-						p2 = kL2[k1_num*Nx+x_num];
-						p3 = kL2[k1_num*Nx+x_num]/D1_arr[a_num*Nk + k_num];
-						p4 = -1.*kL2[k1_num*Nx+x_num]/(1.-pow2(k1dotk2)); //D2 = -(k.L2)/(1 - k1dotk2^2)
-						p5 = -7.*p4/(3.*pow2(D1_arr[a_num*Nk + k_num])); //Expect D2 = -3/7 D1^2 for EdS
+						p1 = D1k[k1_num*Nx+x_num]; //D1(a, k)
+						p2 = kL2[k1_num*Nx+x_num]; //[k.L2](a, k, k1, x)
+						p3 = p2/p1; //[k.L2](a, k, k1, x)/D1(a, k)
+						p4 = -1.*p2/(1.-pow2(k1dotk2)); //D2 = -(k.L2)/(1 - k1dotk2^2)
+						p5 = -7.*p4/(3.*pow2(p1)); //Expect D2 = -3/7 D1^2 for EdS
 
 						/*
 						if( fabs(p5-1.) > 1e-10 ){
@@ -448,17 +349,17 @@ int main(int argc, char* argv[]) {
 						}*/
 
 						//std::cout << "a:" << a_val <<  " k:" << k << " k1:" << k1 << " x:" << x << " k1dotk2:" << k1dotk2 << " D1:" <<  p1 << " kL2:" << p2 << " D2/D1:" << p5 << '\n';
-						std::cout << "a:" << a_val <<  " k:" << k << " k1:" << k1 << " k2:" << k2 << " D1:" <<  p1 << " D2:" << p4 << " D2/D1:" << p5 << '\n';
+						std::cout << "a:" << a_val <<  " k:" << k << " k1:" << k1 << " k2:" << k2 << " x:" << x << " D1:" <<  p1 << " D2:" << p4 << " D2/D1:" << p5 << '\n';
 
-					  // a, k, k1, k2, D2
-					  fprintf(fp,"%e %e %e %e %e \n", a_val, k, k1, k2, p4);
+					  // a, k, k1, k2, D1, D2
+					  fprintf(fp,"%e %e %e %e %e %e \n", a_val, k, k1, k2, p1, p4);
 
 			 	  	}
 			  	}
 			}
 	}
 
-	std::cout << "2ND ORDER COMPLETED" << '\n';
+	//std::cout << "2ND ORDER COMPLETED" << '\n';
 
 	fclose(fp);
 
